e24d18f994be69fa997ff7ae505902712ae83f97
leonardofalmeida <ferreiraleoalmeida@gmail.com>
2019-07-31 08:14:12 -0300

    [WIP] Adiciona cards na home, mudança no layout

--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -1,44 +1,107 @@
 import React, { Component } from 'react';
-import { HomeContainer, HeaderContainer } from './Home.style'
+import { HomeContainer, HeaderContainer, Actions, SearchContainer, ContentContainer } from './Home.style';
 import Header from '../../components/Header/Header.component';
 import Searchbar from '../../components/SearchBar/Searchbar.component';
-import { Checkbox, FormControlLabel } from '@material-ui/core';
 import CheckBox from '../../components/Checkbox/Checkbox.component';
 import toolsServices from '../../services/toolsServices';
+import MyButton from '../../components/Button/Button.component';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tools: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      tools: [],
+      search: '',
+      searchInTags: false,
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  async componentDidMount() {
+    const response = await toolsServices.findAllTools();
+    this.setState({
+      tools: [...response],
+    });
+  }
+
+  handleInputChange = event => {
+    const { target } = event;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSearch = async () => {
+    const { search, searchInTags } = this.state;
+    let response = [];
+
+    /* Verifica se a pesquisa será geral ou por tags */
+    if (searchInTags) {
+      response = await toolsServices.findByTags(search)
+    }
+    if (!searchInTags) {
+      response = await toolsServices.findBySearch(search)
     }
 
-    async componentDidMount() {
-        const response = await toolsServices.findAllTools();
-        this.setState({
-            tools: [...response],
-        })
-    }
+    this.setState({
+      tools: [...response]
+    })
+  }
 
-    render() {
-        const { state: { tools } } = this;
-        return (
-            <HomeContainer>
-                <HeaderContainer>
-                    <Header
-                        titulo='VUTTR'
-                        subtitulo='Very Useful Tools to Remember'
-                    />
-                    <Searchbar></Searchbar>
-                    <CheckBox></CheckBox>
-                </HeaderContainer>
-                <div>
-                    {tools.map(tool => <p>{tool.title}</p>)}
-                </div>
-            </HomeContainer>
-        );
-    }
+  render() {
+    const {
+      state: { tools },
+    } = this;
+    return (
+      <HomeContainer>
+        <HeaderContainer>
+
+          <Header titulo="VUTTR" subtitulo="Very Useful Tools to Remember" />
+          <Actions>
+            <SearchContainer>
+              <Searchbar
+                handleInputChange={this.handleInputChange}
+                handleSearch={this.handleSearch}
+              />
+              <CheckBox
+                handleInputChange={this.handleInputChange}
+                state={this.state}
+              />
+            </SearchContainer>
+
+            <MyButton size='medium' variant='contained' color='primary' name="+ Add" />
+          </Actions>
+
+        </HeaderContainer>
+        <ContentContainer>
+          {tools.map(tool => (
+            <Card key={tool.id} style={{ marginBottom: '10px' }}>
+              <CardContent>
+                <Typography variant="h5" component='a'>
+                  {tool.title}
+                </Typography>
+                <MyButton size='small' name='Remove' color='secondary'>
+                  <DeleteIcon fontSize='small' />
+                </MyButton>
+                <Typography variant="h6" component="h2">
+                  {tool.description}
+                </Typography>
+                <Typography variant="body2">
+                  {tool.tags.map((tag, index) => <span key={index}>{`#${tag} `}</span>)}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </ContentContainer>
+      </HomeContainer>
+    );
+  }
 }
 
 export default Home;
--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
-    height: 100vh;
+    min-height: 100vh;
     width: 100%;
     display: flex;
     flex-direction: column;
@@ -10,5 +10,24 @@ export const HomeContainer = styled.div`
 `;
 
 export const HeaderContainer = styled.header`
+    width: 50%;
+    margin: 10px;
+`;
+
+export const Actions = styled.div`
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const SearchContainer = styled.div`
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const ContentContainer = styled.section`
+    width: 50%;
     margin: 10px;
 `;
