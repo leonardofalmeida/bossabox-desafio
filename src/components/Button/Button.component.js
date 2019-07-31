f8b51e12bc3be5d49a8906b18bea09921a615ee4
leonardo.almeida <leonardoalmeida.gt4w@gmail.com>
2019-07-31 20:31:49 -0300

    Altera proptypes do botão

--- src/components/Button/Button.component.js
+++ src/components/Button/Button.component.js
@@ -3,10 +3,10 @@ import { Button } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 export default function MyButton({
-	variant, color, size, name, children,
+	variant, color, size, name, children, onClick,
 }) {
 	return (
-		<Button size={size} variant={variant} color={color}>
+		<Button onClick={onClick} size={size} variant={variant} color={color}>
 			{children}
 			{name}
 		</Button>
@@ -15,7 +15,7 @@ export default function MyButton({
 
 MyButton.propTypes = {
 	name: PropTypes.string.isRequired,
-	children: PropTypes.node.isRequired,
+	children: PropTypes.node,
 	variant: PropTypes.string,
 	color: PropTypes.string,
 	size: PropTypes.string,
@@ -25,4 +25,5 @@ MyButton.defaultProps = {
 	variant: 'text',
 	color: 'primary',
 	size: 'medium',
+	children: null,
 };
