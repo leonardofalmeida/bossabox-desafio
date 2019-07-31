61e9d1844c2a66a95504315a1f8b953318e9804b
leonardofalmeida <ferreiraleoalmeida@gmail.com>
2019-07-31 08:14:37 -0300

    Cria findBySearch e findByTags

--- src/services/toolsServices.js
+++ src/services/toolsServices.js
@@ -1,8 +1,18 @@
 import api from '../api/api';
 
 export default {
-    async findAllTools() {
-        const { data } = await api.get('/tools');
-        return data;
-    }
-}
+  async findAllTools() {
+    const { data } = await api.get('/tools');
+    return data;
+  },
+
+  async findBySearch(search) {
+    const { data } = await api.get(`/tools?q=${search}`);
+    return data;
+  },
+
+  async findByTags(search) {
+    const { data } = await api.get(`/tools?tags_like=${search}`);
+    return data;
+  },
+};
