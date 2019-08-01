49256ff6c8da37d1e6fe88047032eb677cfbf2e0
leonardo.almeida <leonardoalmeida.gt4w@gmail.com>
2019-08-01 19:43:06 -0300

    Alterado as rotas no serviço para as contasntes

--- src/services/toolsServices.js
+++ src/services/toolsServices.js
@@ -1,28 +1,29 @@
 import api from '../api/api';
+import { TOOLS } from '../constants/routes';
 
 export default {
 	async findAllTools() {
-		const { data } = await api.get('/tools');
+		const { data } = await api.get(TOOLS.FIND_ALL);
 		return data;
 	},
 
 	async findBySearch(search) {
-		const { data } = await api.get(`/tools?q=${search}`);
+		const { data } = await api.get(TOOLS.FIND_BY_SEARCH.replace('{search}', search));
 		return data;
 	},
 
 	async findByTags(search) {
-		const { data } = await api.get(`/tools?tags_like=${search}`);
+		const { data } = await api.get(TOOLS.FIND_BY_TAGS.replace('{search}', search));
 		return data;
 	},
 
 	async deleteTool(id) {
-		const response = await api.delete(`/tools/${id}`);
+		const response = await api.delete(TOOLS.DELETE.replace('{id}', id));
 		return response;
 	},
 
 	async saveTool(tool) {
-		const response = await api.post(`/tools`, tool);
+		const response = await api.post(TOOLS.SAVE, tool);
 		return response;
 	},
 };
